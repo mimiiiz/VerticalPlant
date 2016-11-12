d86c3ca4b0bdf728d91ba91a49391f6948ee055d
Marktrs <marktrs69@gmail.com>
2016-11-12 23:04:59 +0700

    Finished toolbar

--- Verticalplant/app/src/main/java/com/example/mark/verticalplant/MainActivity.java
+++ Verticalplant/app/src/main/java/com/example/mark/verticalplant/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.mark.verticalplant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Mark");
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_add:
+                addNewPlant();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void addNewPlant() {
+        //start Add plant activity
     }
 }
